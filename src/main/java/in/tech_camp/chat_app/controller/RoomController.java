072181ff31675ca46634be0156e8956aadab8a8d
erika-hashi <e.hashi@infomart.co.jp>
2025-12-02 14:42:10 +0900

    画面遷移の実装

--- src/main/java/in/tech_camp/chat_app/controller/RoomController.java
+++ src/main/java/in/tech_camp/chat_app/controller/RoomController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import in.tech_camp.chat_app.custom_user.CustomUserDetail;
 import in.tech_camp.chat_app.entity.RoomEntity;
+import in.tech_camp.chat_app.entity.RoomUserEntity;
 import in.tech_camp.chat_app.entity.UserEntity;
 import in.tech_camp.chat_app.form.RoomForm;
 import in.tech_camp.chat_app.repository.RoomRepository;
@@ -64,7 +65,18 @@ public class RoomController {
       model.addAttribute("roomForm", new RoomForm());
       return "rooms/new";
     }
-
     return "redirect:/";
+  }
+
+  @GetMapping("/")
+  public String index(@AuthenticationPrincipal CustomUserDetail currentUser, Model model) {
+    UserEntity user = userRepository.findById(currentUser.getId());
+    model.addAttribute("user", user);
+    List<RoomUserEntity> roomUserEntities = roomUserRepository.findByUserId(currentUser.getId());
+    List<RoomEntity> roomList = roomUserEntities.stream()
+        .map(RoomUserEntity::getRoom)
+        .collect(Collectors.toList());
+    model.addAttribute("rooms", roomList);
+    return "rooms/index";
   }
 }
